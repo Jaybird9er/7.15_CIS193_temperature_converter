0bba7672b5180c7697fe79995040713cffa282bd
Jamey Kirk <jameykirk@gmail.com>
2021-04-20 23:47:35 +0900

    domLoaded function done

--- convert.js
+++ convert.js
@@ -1,7 +1,19 @@
 window.addEventListener("DOMContentLoaded", domLoaded);
 
 function domLoaded() {
-   // TODO: Complete the function
+   var cVal = document.getElementById("cInput");
+   var fVal = document.getElementById("fInput");
+
+   document.getElementById("convertButton").addEventListener("click", function () {
+      if (cVal.value === "") { // farenheit to celsius
+         var fNum = parseFloat(fVal.value);
+         cVal.value = convertFtoC(fNum);
+      }
+      else if (fVal.value === "") { // celsius to farenheit
+         var cNum = parseFloat(cVal.value);
+         fVal.value = convertCtoF(cNum);
+      }
+   });
 }
 
 function convertCtoF(degreesCelsius) {
